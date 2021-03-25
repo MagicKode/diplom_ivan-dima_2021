8c76346e8d0109ca014694cd5e06e276d6b95026
MagicKode <dmi6756@yandex.ru>
2021-03-25 13:40:40 +0300

    first commit

---
+++ src/main/java/tms/diplom_ivan_dima/Diplom2021Application.java
@@ -0,0 +1,13 @@
+package tms.diplom_ivan_dima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diplom2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Diplom2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/tms/diplom_ivan_dima/Diplom2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package tms.diplom_ivan_dima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diplom2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
